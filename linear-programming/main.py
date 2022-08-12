96b5f4173488e610cce60d116994989a90af9165
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2022-08-12 15:16:12 +0900

    moved

--- linear-programming/main.py
+++ linear-programming/main.py
@@ -9,6 +9,22 @@ def main():
     x = solver.NumVar(0, 1, 'x')
     y = solver.NumVar(0, 2, 'y')
     print('Number of variables =', solver.NumVariables())
+
+    # Create a linear constraint, 0 <= x + y <= 2.
+    ct = solver.Constraint(0, 2, 'ct')
+    ct.SetCoefficient(x, 1)
+    ct.SetCoefficient(y, 1)
+    print('Number of constraints = ', solver.NumConstraints())
+
+    # Create the objective function, 3 * x + y.
+    objective = solver.Objective()
+    objective.SetCoefficient(x, 3)
+    objective.SetCoefficient(y, 1)
+    objective.SetMaximization()
+    solver.Solve()
+    print('Solution:')
+    print('Objective value =', objective.Value())
+    print('y =', y.solution_value())
 if __name__ == '__main__':
    pywrapinit.CppBridge.InitLogging('basic_example.py')
    cpp_flags = pywrapinit.CppFlags()
